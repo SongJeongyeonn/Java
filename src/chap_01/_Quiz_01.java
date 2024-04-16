aa563d5d745ff08c29811be4db4038cbc3f62251
Jeongyeon_Song <songj239587@gmail.com>
2024-04-16 20:32:13 +0900

    3 commit

---
+++ src/chap_01/_Quiz_01.java
@@ -0,0 +1,17 @@
+package chap_01;
+/*
+(실행 결과)
+상암08 버스
+약 3분 후 도착
+남은거리 1.2km
+*/
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String num = "상암08";
+        int min = 3;
+        float km = 1.2F;
+        System.out.println(num + "번 버스");
+        System.out.println("약 " + min + "분 후 도착");
+        System.out.println("남은 거리 " + km + "km");
+    }
+}
