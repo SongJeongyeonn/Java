dba26bfd40d62ecc7d5c339e09388b04bc1e8172
Jeongyeon_Song <songj239587@gmail.com>
2024-06-16 02:16:36 +0900

    Inheritance.example1 finished commit

---
+++ src/org/opentutorials/javatutorials/Inheritance/example1/CalculatorDemo1.java
@@ -0,0 +1,29 @@
+package org.opentutorials.javatutorials.Inheritance.example1;
+class Calculator{
+    int left, right;
+
+    public void setOprands(int left, int right){
+        this.left = left;
+        this.right = right;
+    }
+    public void sum(){
+        System.out.println(this.left+this.right);
+    }
+    public void avg(){
+        System.out.println((this.left+this.right)/2);
+    }
+}
+class  SubstractionableCalculator extends Calculator{ // SC(하위 클래스)기 C(상위 클래스)를 상속한다.
+    public void substract(){
+        System.out.println(this.left - this.right);
+    }
+}
+public class CalculatorDemo1 {
+    public static void main(String[] args) {
+        SubstractionableCalculator c1 = new SubstractionableCalculator();
+        c1.setOprands(10, 20);
+        c1.sum();
+        c1.avg();
+        c1.substract();
+    }
+}
