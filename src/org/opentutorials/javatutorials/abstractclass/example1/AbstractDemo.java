1f5ec08b7a2665bdaedf33b9a8ef80d76aa18a1d
Jeongyeon_Song <songj239587@gmail.com>
2024-10-28 23:53:19 +0900

    10/28

---
+++ src/org/opentutorials/javatutorials/abstractclass/example1/AbstractDemo.java
@@ -0,0 +1,23 @@
+package org.opentutorials.javatutorials.abstractclass.example1;
+abstract class A{ // 추상 클래스 A
+    public abstract int b(); // 추상메서드는 본체가 없다.(사용을 위해서는 )
+    /* 본체가 있는 메서드는 abstract 키워드를 가질 수 없다.
+    public abstract int c() {System.out.println("Hello")}  메서드의 구체적인 로직이 담겨있으므로 abstract로 사용 불가
+    추상 클래스 내에는 추상 메서드가 아닌 메서드가 존재할 수 있다.
+     */
+    public void d(){
+        System.out.println("world");
+    }
+}
+class B extends A{
+    public int b(){ // A 클래스에서 b 메서드가 추상 메서드로, 오버라이딩을 해야 사용할 수 있다.
+        return 1;
+    }
+
+}
+public class AbstractDemo {
+    public static void main(String[] args) {
+        //A obj = new A();  abstract 클래스로 인스턴스화 할 수 없다.
+        B obj = new B();
+    }
+}
